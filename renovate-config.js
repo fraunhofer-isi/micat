1f7d72c36aceb17f243baeba97436808a55e5be2
stefan <eis@isi.fhg.de>
2023-07-28 13:48:23 +0200

    trying to migrate config #3

--- renovate-config.js
+++ renovate-config.js
@@ -1,25 +1,25 @@
 module.exports = {
-  username: 'renovate-release',
-  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
-  onboarding: false,
-  requireConfig: false,
-  platform: 'github',
-  includeForks: true,
-  dryRun: 'full',
-  repositories: ['fraunhofer-isi/micat'],
-  packageRules: [
+  "username": "renovate-release",
+  "gitAuthor": "Renovate Bot <bot@renovateapp.com>",
+  "onboarding": false,
+  "requireConfig": "optional",
+  "platform": "github",
+  "forkProcessing": "enabled",
+  "dryRun": "full",
+  "repositories": ["fraunhofer-isi/micat"],
+  "packageRules": [
     {
-      description: 'lockFileMaintenance',
-      matchUpdateTypes: [
-        'pin',
-        'digest',
-        'patch',
-        'minor',
-        'major',
-        'lockFileMaintenance',
-      ],
-      dependencyDashboardApproval: false,
-      stabilityDays: 0,
-    },
-  ],
+     "description": "lockFileMaintenance",
+     "matchUpdateTypes": [
+       "pin",
+       "digest",
+       "patch",
+       "minor",
+       "major",
+       "lockFileMaintenance"
+     ],
+     "dependencyDashboardApproval": false,
+     "minimumReleaseAge": null
+    }
+  ]
 };
