938dc4a8d5a7342280386a7ba7075163ec3594c0
stefaneidelloth <eis@isi.fhg.de>
2023-07-31 11:16:22 +0200

    increased test coverage #4

--- back_end/test/test_main.py
+++ back_end/test/test_main.py
@@ -1,1 +1,8 @@
-print('Hello world')
+from main import main
+from mock import MagicMock, call, patch
+
+
+@patch('builtins.print')
+def test_main(mocked_print):
+    main()
+    mocked_print.assert_called()
