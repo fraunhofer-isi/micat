fef1ac4fbcd7cc9fb2465bf1756e2d6a1a778568
stefaneidelloth <eis@isi.fhg.de>
2023-07-31 11:32:24 +0200

    fixed warning #4

--- front_end/test/pages/[id-mode]/[id-region]/index.test.js
+++ front_end/test/pages/[id-mode]/[id-region]/index.test.js
@@ -1,6 +1,6 @@
 import Index from '../../../../src/pages/[id-mode]/[id-region]/index';
 
 it('construction', () => {
-  const result = Index({})
+  const result = Index({});
   expect(result).toBe('Hello world');
 });
