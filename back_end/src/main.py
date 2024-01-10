8218b1b986702e04aa9c924ae298027f7f7dded1
stefaneidelloth <eis@isi.fhg.de>
2024-01-10 17:10:02 +0100

    used pandas to influence requirements #1

--- back_end/src/main.py
+++ back_end/src/main.py
@@ -2,8 +2,13 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+import pandas as pd
+
+
 def main():
-    print('Hello world')
+    df = pd.DataFrame([{'id_foo': 1, '2000': 1}])
+    value = df['2000'][0]
+    print('Hello world ' + str(value))
 
 
 if __name__ == '__main__':
