5243a2ad3a5d2f8411c4faee21120b726c5f5eeb
stefaneidelloth <eis@isi.fhg.de>
2024-05-02 18:11:07 +0200

    trying to run renovate for actions #14

--- renovate_config.js
+++ renovate_config.js
@@ -9,8 +9,8 @@
 // https://docs.renovatebot.com/self-hosted-configuration/
 
 module.exports = {
-  "username": "renovate-release",
-  "gitAuthor": "Renovate Bot <bot@renovateapp.com>",
+  "$schema": "https://docs.renovatebot.com/renovate-schema.json",
+  "extends": ["config:recommended"],
   "onboarding": false,
   "requireConfig": "optional",
   "platform": "github",
