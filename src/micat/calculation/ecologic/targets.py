d0674e8398b9015be91c53f6ba47ad13741da7aa
Frederic <frederic.berger@isi.fraunhofer.de>
2024-09-06 15:52:51 +0200

    Revert buggy scripts

--- src/micat/calculation/ecologic/targets.py
+++ src/micat/calculation/ecologic/targets.py
@@ -5,12 +5,10 @@
 # https://gitlab.cc-asp.fraunhofer.de/isi/micat/-/issues/44
 # https://gitlab.cc-asp.fraunhofer.de/isi/micat/-/issues/296
 
-from micat.table.table import Table
-
 
 def impact_on_res_targets(
-        gross_available_energy,
-        total_primary_energy_saving,
+    gross_available_energy,
+    total_primary_energy_saving,
 ):
     renewable_gae = gross_available_energy.reduce('id_primary_energy_carrier', [4, 5])
     renewable_gae_sum = renewable_gae.sum()
@@ -20,24 +18,16 @@ def impact_on_res_targets(
 
     sum_difference = total_gae_sum - total_pes_sum
 
-    targets_table_baseline = _targets_table(renewable_gae_sum, total_gae_sum, 'Without savings')
-    targets_table_baseline['case'] = 'baseline'
-    targets_table = _targets_table(renewable_gae_sum, sum_difference, 'With savings')
-    targets_table['case'] = 'with_savings'
-
-    result = Table.concat([targets_table_baseline, targets_table])
-    return result
+    change_of_share = renewable_gae_sum / sum_difference - renewable_gae_sum / total_gae_sum
+    return change_of_share
 
 
 def impact_on_res_targets_monetization(
-        targets_tables,
-        gross_available_energy,
-        total_primary_energy_saving,
-        cost_of_res_statistical_transfer,
+    change_of_share,
+    gross_available_energy,
+    total_primary_energy_saving,
+    cost_of_res_statistical_transfer,
 ):
-    change_of_share = targets_tables[targets_tables['case'] == 'with_savings'] - targets_tables[
-        targets_tables['case'] == 'baseline']
-
     total_gae_sum = gross_available_energy.sum()
 
     total_pes_sum = total_primary_energy_saving.aggregate_to(['id_measure'])
@@ -47,13 +37,3 @@ def impact_on_res_targets_monetization(
     cost_of_statistical_transfer = change_of_share * sum_difference * cost_of_res_statistical_transfer
 
     return cost_of_statistical_transfer
-
-
-def _targets_table(
-        ren_gae,
-        tot_gae,
-        label,
-):
-    targets_series = ren_gae / tot_gae
-    targets_table = targets_series.transpose('label', label)
-    return targets_table
