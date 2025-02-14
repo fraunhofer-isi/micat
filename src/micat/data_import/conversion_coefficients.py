fix: fixed heat saving refernece in primary energy savings calculation

src/micat/data_import/conversion_coefficients.py
@@ -10,13 +10,13 @@ from micat.table.table import Table
 
 def conversion_coefficients(inout):
     # pylint: disable=too-many-locals
-    heat_in_carriers = inout['heat_in']
-    heat_out = inout['heat_out']
-    electricity_in_carriers = inout['electricity_in']
-    electricity_out = inout['electricity_out']
-    chp_in_carriers = inout['chp_in']
-    chp_heat_out = inout['chp_heat_out']
-    chp_electricity_out = inout['chp_electricity_out']
+    heat_in_carriers = inout["heat_in"]
+    heat_out = inout["heat_out"]
+    electricity_in_carriers = inout["electricity_in"]
+    electricity_out = inout["electricity_out"]
+    chp_in_carriers = inout["chp_in"]
+    chp_heat_out = inout["chp_heat_out"]
+    chp_electricity_out = inout["chp_electricity_out"]
 
     sigma_heat = _input_output_ratio(heat_in_carriers, heat_out)
     sigma_electricity = _input_output_ratio(electricity_in_carriers, electricity_out)
@@ -40,22 +40,22 @@ def conversion_coefficients(inout):
     )
 
     table = None
-    primary_energy_carrier_ids = heat_in_carriers.unique_index_values('id_primary_energy_carrier')
+    primary_energy_carrier_ids = heat_in_carriers.unique_index_values("id_primary_energy_carrier")
     for carrier_id in primary_energy_carrier_ids:
-        heat_in = heat_in_carriers.reduce('id_primary_energy_carrier', carrier_id)
-        chp_in = chp_in_carriers.reduce('id_primary_energy_carrier', carrier_id)
+        heat_in = heat_in_carriers.reduce("id_primary_energy_carrier", carrier_id)
+        chp_in = chp_in_carriers.reduce("id_primary_energy_carrier", carrier_id)
         k_heat = _coefficient(heat_in, tau_heat, chp_in, heat_out, chp_heat_out)
-        k_heat = k_heat.insert_index_column('id_parameter', 1, 20)
-        k_heat = k_heat.insert_index_column('id_primary_energy_carrier', 2, carrier_id)
+        k_heat = k_heat.insert_index_column("id_parameter", 1, 20)
+        k_heat = k_heat.insert_index_column("id_primary_energy_carrier", 2, carrier_id)
         if table is None:
             table = k_heat
         else:
             table = Table.concat([table, k_heat])
 
-        electricity_in = electricity_in_carriers.reduce('id_primary_energy_carrier', carrier_id)
+        electricity_in = electricity_in_carriers.reduce("id_primary_energy_carrier", carrier_id)
         k_electricity = _coefficient(electricity_in, tau_electricity, chp_in, electricity_out, chp_electricity_out)
-        k_electricity = k_electricity.insert_index_column('id_parameter', 1, 21)
-        k_electricity = k_electricity.insert_index_column('id_primary_energy_carrier', 2, carrier_id)
+        k_electricity = k_electricity.insert_index_column("id_parameter", 1, 21)
+        k_electricity = k_electricity.insert_index_column("id_primary_energy_carrier", 2, carrier_id)
         table = Table.concat([table, k_electricity])
 
     return table
@@ -94,7 +94,7 @@ def _input_output_ratio(
     energy_input_table,
     energy_output_table,
 ):
-    total_energy_input = energy_input_table.aggregate_to(['id_region'])
+    total_energy_input = energy_input_table.aggregate_to(["id_region"])
     normal_result = total_energy_input.divide_without_checks(energy_output_table)
     normal_result = normal_result.replace([np.inf, -np.inf], np.nan)
     mean = normal_result.mean()