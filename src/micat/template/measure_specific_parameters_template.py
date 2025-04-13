3b4f3ad69ee47e3ca84d44b3c7a3eede56d549a2
Tyrdall <536181+Tyrdall@users.noreply.github.com>
2025-04-13 17:00:30 +0000

    create copyright and license annotations with reuse tool

--- src/micat/template/measure_specific_parameters_template.py
+++ src/micat/template/measure_specific_parameters_template.py
@@ -1,6 +1,7 @@
 # © 2024 - 2025 Fraunhofer-Gesellschaft e.V., München
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
+
 from copy import copy
 
 import numpy as np
--- test/micat/template/test_validators.py
+++ test/micat/template/test_validators.py
@@ -1,6 +1,7 @@
 # © 2024 - 2025 Fraunhofer-Gesellschaft e.V., München
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
+
 from datetime import datetime
 from micat.template import mocks, validators, xlsx_utils
 
