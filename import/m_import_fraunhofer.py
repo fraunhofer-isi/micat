745cd6614c436018776b4006c989f5a0a0a18c86
Tobias Lorenz <tobias.lorenz@rtl.de>
2025-06-05 12:47:11 +0200

    fix: updated public database

--- import/m_import_fraunhofer.py
+++ import/m_import_fraunhofer.py
@@ -15,14 +15,14 @@ def main():
 
     database_import = DatabaseImport(public_database_path)
 
-    import_path = raw_data_path + '/fraunhofer'
+    import_path = raw_data_path + "/fraunhofer"
 
     cost_of_res_statistical_transfer_file_path = import_path + "/statistical_transfers.xlsx"
     raw_cost_of_res_statistical_transfer = pd.read_excel(cost_of_res_statistical_transfer_file_path)
     cost_of_res_statistical_transfer = Table(raw_cost_of_res_statistical_transfer)
     id_parameter = 61
     fraunhofer_constant_parameters = cost_of_res_statistical_transfer.insert_index_column(
-        'id_parameter',
+        "id_parameter",
         1,
         id_parameter,
     )
@@ -32,13 +32,14 @@ def main():
     capacity_factors = Table(raw_capacity_factors)
     id_parameter = 64
     fraunhofer_capacity_factors = capacity_factors.insert_index_column(
-        'id_parameter',
+        "id_parameter",
         1,
         id_parameter,
     )
 
-    database_import.write_to_sqlite(fraunhofer_constant_parameters, 'fraunhofer_constant_parameters')
-    database_import.write_to_sqlite(fraunhofer_capacity_factors, 'fraunhofer_capacity_factors')
+    database_import.write_to_sqlite(fraunhofer_constant_parameters, "fraunhofer_constant_parameters")
+
+    database_import.write_to_sqlite(fraunhofer_capacity_factors, "fraunhofer_capacity_factors")
     # In case of issue with AnnualSeries, comment out "self._table_validator.validate(sorted_table, details)" in the
     # write_to_sqlite method of the DatabaseImport class
 
